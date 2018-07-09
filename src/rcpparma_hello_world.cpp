325aef802279a6441e61f26fa5108a176e6ca14a
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2018-07-09 15:16:13 +0300

    include omp.h

--- src/rcpparma_hello_world.cpp
+++ src/rcpparma_hello_world.cpp
@@ -3,6 +3,10 @@
 // we only include RcppArmadillo.h which pulls Rcpp.h in for us
 #include "RcppArmadillo.h"
 
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
+
 // via the depends attribute we tell Rcpp to create hooks for
 // RcppArmadillo so that the build process will know what to do
 //
